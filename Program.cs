ff0cc20eda8750ed17b27580fa63b52c70492550
rufatgit <93449554+rufatgit@users.noreply.github.com>
2023-02-20 12:21:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+Console.WriteLine("Enter 2 numbers");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+
+if (a==b)
+{
+    Console.WriteLine("Equal");
+}
+else
+{
+    Console.WriteLine("Different");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
